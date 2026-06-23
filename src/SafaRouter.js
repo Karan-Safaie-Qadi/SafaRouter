f3cd91cdb2fd5fd25ea49e2ae180e1464b2956b2
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-23 11:53:22 +0330

    v1.2.5: fix static version string in SafaRouter.js

--- src/SafaRouter.js
+++ src/SafaRouter.js
@@ -10,8 +10,8 @@ import { EVENTS, DEFAULT_CONFIG } from './constants.js'
 import { RouteLoadError, SafaError } from './errors.js'
 
 export class SafaRouter {
-  static version = '1.2.4'
-  static VERSION = '1.2.4'
+  static version = '1.2.5'
+  static VERSION = '1.2.5'
 
   constructor(options = {}) {
     this.config = { ...DEFAULT_CONFIG, ...options }
