c9861c1c1d76093b0730986537f97114bc517d8f
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-23 10:39:51 +0330

    feat: add middleware priority with useNamed insertBefore insertAfter

--- src/MiddlewareChain.js
+++ src/MiddlewareChain.js
@@ -3,12 +3,20 @@ export class MiddlewareChain {
     this._stack = []
   }
 
-  use(fn) {
+  use(fn, priority = 0) {
     if (typeof fn !== 'function') {
       throw new Error('Middleware must be a function')
     }
+    fn._priority = priority
+    fn._name = fn.name || ''
     this._stack.push(fn)
+    this._stack.sort((a, b) => (b._priority || 0) - (a._priority || 0))
     return this
+  }
+
+  useNamed(name, fn, priority = 0) {
+    fn._name = name
+    return this.use(fn, priority)
   }
 
   async run(ctx) {
@@ -31,17 +39,43 @@ export class MiddlewareChain {
   }
 
   remove(fn) {
-    this._stack = this._stack.filter(f => f !== fn)
+    if (typeof fn === 'string') {
+      this._stack = this._stack.filter(f => f._name !== fn)
+    } else {
+      this._stack = this._stack.filter(f => f !== fn)
+    }
+    return this
+  }
+
+  insertBefore(refName, fn, priority = 0) {
+    const idx = this._stack.findIndex(f => f._name === refName)
+    if (idx === -1) { this.use(fn, priority); return this }
+    fn._priority = priority
+    fn._name = fn.name || ''
+    this._stack.splice(idx, 0, fn)
+    return this
+  }
+
+  insertAfter(refName, fn, priority = 0) {
+    const idx = this._stack.findIndex(f => f._name === refName)
+    if (idx === -1) { this.use(fn, priority); return this }
+    fn._priority = priority
+    fn._name = fn.name || ''
+    this._stack.splice(idx + 1, 0, fn)
     return this
   }
 
   clone() {
     const chain = new MiddlewareChain()
-    chain._stack = [...this._stack]
+    chain._stack = this._stack.map(f => f)
     return chain
   }
 
   get length() {
     return this._stack.length
+  }
+
+  get stack() {
+    return this._stack.map(f => ({ name: f._name, priority: f._priority || 0 }))
   }
 }
